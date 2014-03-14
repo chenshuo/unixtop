512edbf576f83be592763d499909589b707cdb12
Shuo Chen <chenshuo@chenshuo.com>
2014-03-13 20:15:34 -0700

    import top-3.8beta1

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ unixtop
 =======
 
 Unix Top is written by William  LeFebvre, see http://www.unixtop.org
+
+This is a fork for Linux with better threading support, by Shuo Chen.
---
+++ sigdesc.h
@@ -0,0 +1,11 @@
+/* This file was automatically generated */
+/* by the awk script "sigconv.awk".      */
+
+struct sigdesc {
+    char *name;
+    int  number;
+};
+
+struct sigdesc sigdesc[] = {
+    { NULL,	 0 }
+};
